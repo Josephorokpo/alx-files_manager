ce26f10a046dc74a0e050ed4a86ea567a50513e5
josephorokpo <josephorokpo@gmail.com>
2024-06-27 04:52:35 -0700

    redisClient.test.js

---
+++ test/redisClient.test.js
@@ -0,0 +1,26 @@
+// test/redisClient.test.js
+const { expect } = require('chai');
+const redisClient = require('../utils/redis');
+
+describe('redisClient', () => {
+  before((done) => {
+    redisClient.client.on('connect', done);
+  });
+
+  it('should be alive', () => {
+    expect(redisClient.isAlive()).to.be.true;
+  });
+
+  it('should set and get a key', async () => {
+    await redisClient.set('test_key', 'test_value', 10);
+    const value = await redisClient.get('test_key');
+    expect(value).to.equal('test_value');
+  });
+
+  it('should delete a key', async () => {
+    await redisClient.set('test_key', 'test_value', 10);
+    await redisClient.del('test_key');
+    const value = await redisClient.get('test_key');
+    expect(value).to.be.null;
+  });
+});
