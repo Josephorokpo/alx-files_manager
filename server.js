4f12fb16f8b2ed89776b5e34643a464fcdeb4760
josephorokpo <josephorokpo@gmail.com>
2024-06-27 05:16:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// server.js
-
-const express = require('express');
-const routes = require('./routes/index');
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes/index';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(routes);
+
 const port = process.env.PORT || 5000;
-
-app.use(express.json()); // Add this line to parse JSON request bodies
-app.use('/', routes);
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+export default app;
